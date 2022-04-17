a6d91a3a1b3e89e4ce3b4e08bc537766e20c3627
Mert <mertgenc8@gmail.com>
2022-04-17 17:02:37 +0300

    move front to new repo

--- nextjs-client/lib/utilFunctions.ts
+++
@@ -1,7 +0,0 @@
-export function getEnumKeyByEnumValue(
-  myEnum: any,
-  enumValue: number | string
-): string {
-  let keys = Object.keys(myEnum).filter((x) => myEnum[x] == enumValue);
-  return keys.length > 0 ? keys[0] : "";
-}
